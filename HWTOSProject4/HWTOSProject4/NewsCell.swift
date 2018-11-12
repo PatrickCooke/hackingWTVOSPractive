0ea9035c1a35a0c2d134d0424d28377d4d301074
Patrick Cooke <cookepa1@gmail.com>
2018-11-12 13:42:25 -0500

    Began Project 4 - News

---
+++ HWTOSProject4/HWTOSProject4/NewsCell.swift
@@ -0,0 +1,39 @@
+//
+//  NewsCell.swift
+//  HWTOSProject4
+//
+//  Created by Patrick Cooke on 11/12/18.
+//  Copyright © 2018 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    
+    @IBOutlet var unfocusedConstraint: NSLayoutConstraint!
+    var focusedConstraint: NSLayoutConstraint!
+
+    
+    override func awakeFromNib() {
+        focusedConstraint = textLabel.topAnchor.constraint(equalTo: imageView.focusedFrameGuide.bottomAnchor, constant: 15)
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        focusedConstraint.isActive = isFocused
+        unfocusedConstraint.isActive = !isFocused
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        setNeedsUpdateConstraints()
+        
+        coordinator.addCoordinatedAnimations({
+            self.layoutIfNeeded()
+        })
+    }
+    
+}
---
+++ HWTOSProject4/HWTOSProject4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HWTOSProject4
+//
+//  Created by Patrick Cooke on 11/12/18.
+//  Copyright © 2018 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+
+}
+
