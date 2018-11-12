d41a4c254433f7f6e00513e3af3c635d78929616
Patrick Cooke <cookepa1@gmail.com>
2018-11-12 10:36:19 -0500

    Began Project3: Focus

---
+++ HWTOSProject3/HWTOSProject3/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HWTOSProject3
+//
+//  Created by Patrick Cooke on 11/12/18.
+//  Copyright © 2018 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textFieldTip: UILabel!
+    @IBOutlet var nextButton: UIButton!
+    var focusGuide: UIFocusGuide
+    
+    @IBAction func showAlert(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
