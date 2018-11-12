0d759d862d24feedbde11a5a54df70da0015337e
Patrick Cooke <cookepa1@gmail.com>
2018-11-12 12:01:55 -0500

    Completed project 3
    learned about focus in the context of TVOS, how to create a focus guide to create an intended path for users to follow, and how to bridge gaps if screen elements are inline

--- HWTOSProject3/HWTOSProject3/ViewController.swift
+++ HWTOSProject3/HWTOSProject3/ViewController.swift
@@ -13,16 +13,48 @@ class ViewController: UIViewController {
     @IBOutlet var textField: UITextField!
     @IBOutlet var textFieldTip: UILabel!
     @IBOutlet var nextButton: UIButton!
-    var focusGuide: UIFocusGuide
+    var focusGuide: UIFocusGuide!
     
     @IBAction func showAlert(_ sender: Any) {
+        let ac = UIAlertController(title: "Hello", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        focusGuide = UIFocusGuide()
+        view.addLayoutGuide(focusGuide)
+        focusGuide.topAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
+        focusGuide.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        focusGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        focusGuide.preferredFocusEnvironments = [nextButton]
+        restoresFocusAfterTransition = false
     }
 
-
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        if context.nextFocusedView == textField {
+            focusGuide.preferredFocusEnvironments = [nextButton]
+        } else if context.nextFocusedView == nextButton {
+            focusGuide.preferredFocusEnvironments = [textField]
+        }
+        
+        if context.nextFocusedView == textField {
+            coordinator.addCoordinatedAnimations({
+                self.textFieldTip.alpha = 1
+            })
+        } else if context.previouslyFocusedView == textField {
+            coordinator.addCoordinatedAnimations({
+                self.textFieldTip.alpha = 0
+            })
+        }
+//        if let focusedView = UIScreen.main.focusedView {
+//            print(focusedView)
+//        }
+    }
+    
+    override var preferredFocusEnvironments: [UIFocusEnvironment] {
+        return [textField]
+    }
 }
-
