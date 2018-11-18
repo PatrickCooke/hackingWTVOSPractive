87a8d7412a757fb3b8a6c0874ae6bc4c78675fa8
Patrick Cooke <cookepa1@gmail.com>
2018-11-18 16:40:59 -0500

    Beginning work on Project 5

---
+++ HWTVOSProject5/HWTVOSProject5/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCell.swift
+//  HWTVOSProject5
+//
+//  Created by Patrick Cooke on 11/18/18.
+//  Copyright © 2018 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    
+}
---
+++ HWTVOSProject5/HWTVOSProject5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWTVOSProject5
+//
+//  Created by Patrick Cooke on 11/18/18.
+//  Copyright © 2018 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
