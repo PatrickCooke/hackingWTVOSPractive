106b7307cbf0b614321366e15c4e7de0fa05d91e
Patrick Cooke <cookepa1@gmail.com>
2018-11-23 21:54:16 -0500

    stopped Project7 progress for the night

---
+++ HWTVOSProject7/HWTVOSProject7/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  HWTVOSProject7
+//
+//  Created by Patrick Cooke on 11/23/18.
+//  Copyright © 2018 Patrick Cooke. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct City {
+    var name: String
+    var country: String
+    var coordinates: CLLocationCoordinate2D
+}
